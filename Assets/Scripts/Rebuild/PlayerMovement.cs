83bb899c5b49f8a9d38b35aeb325724417b277b1
Mark Plant <gothicsyn@live.co.uk>
2016-12-28 17:03:44 +0000

    Rebuildv1.6
    Started to rebuild ther game, this will be new assets new code,
    hopefully will be a better game than it was before.

---
+++ Assets/Scripts/Rebuild/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField]float movementSpeed = 100;
+	[SerializeField]float turnSpeed = 50;
+
+	Transform myT;
+
+	// Use this for things started as the game loads
+	void Awake () {
+		myT = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Turn ();
+		Thrust ();
+	}
+
+	void Turn () {
+	// input * Turn * Time
+		float yaw = turnSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+		float pitch = turnSpeed * Time.deltaTime * Input.GetAxis("Pitch");
+		float roll = turnSpeed * Time.deltaTime * Input.GetAxis("Roll");
+
+		myT.Rotate (-pitch, yaw, roll);
+	}
+
+	void Thrust () {
+		if(Input.GetAxis("Vertical") > 0)
+		myT.position += myT.forward * movementSpeed *Time.deltaTime * Input.GetAxis("Vertical");
+	}
+}
