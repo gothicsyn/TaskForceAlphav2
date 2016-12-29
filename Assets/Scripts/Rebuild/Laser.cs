6210c6f2f7a62eb96e5a665050aff7bc169b4738
Mark Plant <gothicsyn@live.co.uk>
2016-12-29 17:54:34 +0000

    1.6bUpdateRound2
    Added a basic camera system that follows the ship. Also begun systems
    for weapons and motions. Weapons don't work atm, and the motion trail
    isn't quite right for the moment. Will fix them later.

---
+++ Assets/Scripts/Rebuild/Laser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Laser : MonoBehaviour {
+	[SerializeField]float laserOffTime = .5f;
+	[SerializeField]float maxDistance = 100;
+	LineRenderer lr;
+	bool canFire;
+
+	void Awake () {
+		lr = GetComponent<LineRenderer>();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		lr.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		FireLaser (transform.forward * maxDistance);
+	}
+
+	public void FireLaser (Vector3 targetPosition) {
+		if (canFire) {
+			lr.SetPosition (0, transform.position);
+			lr.SetPosition (1, targetPosition);
+			lr.enabled = true;
+			canFire = false;
+		}
+
+		Invoke ("TurnOffLaser", laserOffTime);
+	}
+
+	void TurnOffLaser () {
+		lr.enabled = false;
+		canFire = true;
+	}
+}
