512e74148ea580b83dbf4bde1349d7368ca68315
Mark Plant <gothicsyn@live.co.uk>
2017-05-08 10:55:11 +0100

    v302Enemy Motion
    {14} Added in enemy movement now, the enemy will follow the player and
    then circle until the player moves again. Alot to go but systems are
    slowly being added. Also changes for Unity 5.6 included.

---
+++ Assets/Scripts/Rebuild/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	[SerializeField]Transform target;
+	[SerializeField]float movementSpeed = 1f;
+	[SerializeField]float rotationalDamp = .5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Turn ();
+		Move();
+	}
+
+	void Turn () {
+		Vector3 pos = target.position - transform.position;
+		Quaternion rotation = Quaternion.LookRotation (pos);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, rotationalDamp * Time.deltaTime);
+	}
+
+	void Move () {
+		transform.position += transform.forward * movementSpeed * Time.deltaTime;
+	}
+}
